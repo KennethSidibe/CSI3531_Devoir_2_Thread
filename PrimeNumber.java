eae67ef3a7e1efa696279730f786306872cbe207
Sidibe <Elstevehcks@gmail.com>
2023-02-21 18:07:55 -0500

    Create class that list all the prime numbers

--- PrimeNumber.java
+++ PrimeNumber.java
@@ -2,9 +2,29 @@ public class PrimeNumber {
     
     public static void main(String[] args) {
 
+        listPrimeNumber(Integer.parseInt(args[0]));
     }
 
-    public void listPrimeNumber(int startNumber) {
+    public static void listPrimeNumber(int startNumber) {
 
+        for(int i=startNumber; i >=1; i--){
+
+            if(isPrimeNumber(i)) {
+                System.out.println(i);
+            }
+
+        }
+    }
+
+    public static boolean isPrimeNumber(int numberToVerify) {
+        
+        for(int i=numberToVerify-1; i>1; i-- ){
+            
+            if (numberToVerify % i ==0) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
